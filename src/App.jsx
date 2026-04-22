608f09ab8736fbfbda2a106f366ca2bc736f12e1
Abdul Kareem <abdulkareemzahir9599@gmail.com>
2026-04-22 08:48:42 +0530

    Add react-toastify for success notifications on photo fetch

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import PhotoCard from './components/PhotoCard';
 import { setError, setLoading, setPhotos, setQuery } from './search.slice';
 import { fetchPhotos } from './unsplash.api';
+import { ToastContainer, Zoom, toast } from 'react-toastify';
 
 const App = () => {
 
@@ -9,11 +10,24 @@ const App = () => {
 
   const dispatch = useDispatch();
 
+  const successToast = () => toast.success(`${query} photos fetched!!!`, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: false,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+    transition: Zoom,
+  });
+
 
   const handleSearch = async () => {
     try {
       dispatch(setLoading());
       dispatch(setPhotos(await fetchPhotos(query)));
+      successToast();
     } catch (err) {
       console.error("Error fetching photos:", err);
       dispatch(setError(err));
@@ -55,6 +69,19 @@ const App = () => {
       ) : (
         <div className='empty'>No photos yet. Try searching!</div>
       )}
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        transition={Zoom}
+      />
     </main>
   )
 }
